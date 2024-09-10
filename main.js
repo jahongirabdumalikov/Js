8ff278a43216ca80180a654bbc4bbe5040ec7285
jahongirabdumalikov <abdumalikovj015@gmail.com>
2024-09-10 11:40:03 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+// fan = "ona tili"
+// fasl = "kuz"
+// ball = "96"
+
+// jami = `dostim ${fan} fanidan ${fasl}gi imtihondan ${ball}ball yegdi`
+// console.log(jami);
+
+
+
+
+
+
+
+let result1 = '3' + 2;
+let result2 = '3' + true;
+let result3 = '3' + undefined;
+let result4 = '3' + null;
+let result5 = '4' - '2';
+let result6 = '4' - 2;
+let result7 = '4' * 2;
+let result8 = '4' / 2;
+let result9 = 'hello' - 'world';
+let result10 = '4' - 'hello';
+let result11 = '4' - true;
+let result12 = 4 + true;
+let result13 = 4 + false;
+let result14 = 4 + null;
+let result15 = 4 - null;
+let result16 = 4 + undefined;
+let result17 = 4 - undefined;
+let result18 = true + undefined;
+let result19 = null + undefined;
+
+console.log('\'3\' + 2 =', result1);
+console.log('\'3\' + true =', result2);
+console.log('\'3\' + undefined =', result3);
+console.log('\'3\' + null =', result4);
+console.log('\'4\' - \'2\' =', result5);
+console.log('\'4\' - 2 =', result6);
+console.log('\'4\' * 2 =', result7);
+console.log('\'4\' / 2 =', result8);
+console.log('\'hello\' - \'world\' =', result9);
+console.log('\'4\' - \'hello\' =', result10);
+console.log('\'4\' - true =', result11);
+console.log('4 + true =', result12);
+console.log('4 + false =', result13);
+console.log('4 + null =', result14);
+console.log('4 - null =', result15);
+console.log('4 + undefined =', result16);
+console.log('4 - undefined =', result17);
+console.log('true + undefined =', result18);
+console.log('null + undefined =', result19);
